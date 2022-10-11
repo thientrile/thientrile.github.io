abc3af4036c1116544de131a87e1e1755f6b2343
thientrile <thientrile2003@gmail.com>
2022-10-11 16:56:58 +0700

    update

--- KT-GHK/assets/js/angular.js
+++
@@ -1,2 +0,0 @@
-var app = angular.module("myapp", []);
-app.controller("myctrl", function ($scope) {});
--- KT-GHK/assets/js/script.js
+++ KT-GHK/assets/js/script.js
@@ -1,6 +1,7 @@
 // change the color when the menu sticky
 if (document.getElementById("nav-menu")) {
 	let navMenu = document.getElementById("nav-menu");
+	// let animate = document.querySelectorAll(".reveal");
 	window.onscroll = function () {
 		if (window.pageYOffset > 0) {
 			navMenu.classList.add("navbar-white");
@@ -13,10 +14,24 @@ if (document.getElementById("nav-menu")) {
 			navMenu.classList.remove("shadow");
 			navMenu.classList.add("navbar-dark");
 		}
+
+		// for (let i = 0; i < animate.length; i++) {
+		// 	let windowHeight = window.innerHeight;
+		// 	let elementTop = animate[i].getBoundingClientRect().top;
+		// 	let elementVisible = 150;
+		// 	if (elementTop < windowHeight - elementVisible) {
+		// 		// animate[i].className.replace("reveal", "");
+		// 		console.log(animate[i].className.search("reveal"));
+		// 	} else {
+		// 		// animate[i].classList.add("reveal");
+		// 		console.log(false);
+		// 	}
+		// }
 	};
 }
 
-// body
+//
+
 // follow number
 if (document.querySelectorAll("#body .follow-point .point")) {
 	let follows = document.querySelectorAll("#body .follow-point .point");
@@ -106,40 +121,26 @@ if (document.querySelectorAll(".typewrite")) {
 var user = [];
 if (document.getElementById("user-login")) {
 	document.getElementById("user-login").onclick = function () {
-		if (user.length == 0) {
-			window.location = "login.html";
-		} else {
-			window.location = "user.html";
-		}
+		window.location = "login.html";
 	};
 }
-if (document.querySelectorAll(".center-wrap")) {
-	let forms = document.querySelectorAll(".center-wrap");
-	for (let i of forms) {
-		i.querySelector(".btn").onclick = function () {
-			const length = new RegExp("(?=.{8,})");
-			if (i.querySelector(".logname")) {
-				let name = i.querySelector(".logname").vaule;
-				let email = i.querySelector(".logemail").value;
-				let pass = i.querySelector(".logpass").value;
-				if (name != "" && email != "" && length.test(pass) == true) {
-					user.push({ name: name, email: email, pass: pass });
-				}
-				window.location = "user.html";
-			} else {
-				let email = i.querySelector(".logemail").value;
-				let pass = i.querySelector(".logpas").value;
-				for (let i of user) {
-					console.log(i.email);
-					// if (email == i.email && pass == i.pass) {
-					// 	window.location = "user.html";
-					// }
-				}
-				console.log(true);
-				console.log(user.length);
-			}
-		};
-	}
+if (document.querySelector("body > div > div > div > div > div > div > div > div.card-front > div > div > button")) {
+	document.querySelector(
+		"body > div > div > div > div > div > div > div > div.card-front > div > div > button"
+	).onclick = function () {
+		console.log(true);
+		let email = document.querySelector(
+			"body > div > div > div > div > div > div > div > div.card-front > div > div > div:nth-child(2) > input"
+		).value;
+		let password = document.querySelector(
+			"body > div > div > div > div > div > div > div > div.card-front > div > div > div.form-group.mt-2 > input"
+		).value;
+		if (email == "admin@admin.com" && password == "admin") {
+			window.location = "user.html";
+		}
+		console.log(email);
+		console.log(password);
+	};
 }
 
 if (document.getElementById("logout")) {
@@ -147,68 +148,50 @@ if (document.getElementById("logout")) {
 		window.location = "login.html";
 	};
 }
-var sl = 0;
-
-var items = [];
-if (document.querySelectorAll(".creative .card")) {
-	let cards = document.querySelectorAll(".creative .card");
-	for (let i of cards) {
-		i.querySelector(".plus").onclick = function () {
-			let img = i.querySelector(".card-img-top").src;
-			let headingNote = i.querySelector(".heading-note").innerHTML;
-			let cardTitle = i.querySelector(".card-title").innerHTML;
-			let cardPrice = i.querySelector(".card-price").innerHTML;
-			if (sl > 0) {
-				let kt = 0;
-				for (let item of items) {
-					if (item.name === cardTitle && item.img === img) {
-						item.amount++;
-						break;
-					} else {
-						kt++;
-						if (kt == items.length) {
-							items.push({
-								name: cardTitle,
-								price: cardPrice,
-								img: img,
-								headingNote: headingNote,
-								amount: 1,
-							});
+if (document.querySelector("#nav-menu > div > div.d-flex > a:nth-child(2) > i")) {
+	var sl = 0;
+	var items = [];
+	if (document.querySelectorAll(".creative .card")) {
+		let cards = document.querySelectorAll(".creative .card");
+		for (let i of cards) {
+			i.querySelector(".plus").onclick = function () {
+				let img = i.querySelector(".card-img-top").src;
+				let headingNote = i.querySelector(".heading-note").innerHTML;
+				let cardTitle = i.querySelector(".card-title").innerHTML;
+				let cardPrice = i.querySelector(".card-price").innerHTML;
+				if (sl > 0) {
+					let kt = 0;
+					for (let item of items) {
+						if (item.name === cardTitle && item.img === img) {
+							item.amount++;
 							break;
+						} else {
+							kt++;
+							if (kt == items.length) {
+								items.push({
+									name: cardTitle,
+									price: cardPrice,
+									img: img,
+									headingNote: headingNote,
+									amount: 1,
+								});
+								break;
+							}
 						}
 					}
+				} else {
+					items.push({ name: cardTitle, price: cardPrice, img: img, headingNote: headingNote, amount: 1 });
 				}
-			} else {
-				items.push({ name: cardTitle, price: cardPrice, img: img, headingNote: headingNote, amount: 1 });
-			}
-			sl++;
+				sl++;
 
-			document.querySelector(".ng-binding").innerHTML = sl;
-		};
-		i.querySelector(".cart").onclick = function () {
-			let img = i.querySelector(".card-img-top").src;
-			let headingNote = i.querySelector(".heading-note").innerHTML;
-			let cardTitle = i.querySelector(".card-title").innerHTML;
-			let cardPrice = i.querySelector(".card-price").innerHTML;
-			document.querySelector("#buy-modal > div > div > div.modal-body").innerHTML = `<div class="container">
-			<div class="row">
-				<div class="col-6">
-					<img src="${img}" alt="" style="width:100%"/>
-				</div>
-				<div class="col-6">
-					<h3>${headingNote}</h3>
-					<h5>${cardTitle}</h5>
-					<p>${cardPrice}</p>
-					<button type="button" class="btn btn-primary" id="add-card" 
-					data-bs-toggle="modal" data-bs-target="#add">Add To Card</button>
-			<button type="button" class="btn btn-danger">Buy</button>
-				</div>
-			</div>
-			
-		</div>
-			`;
-			document.querySelector("#add-card").onclick = function () {
-				document.querySelector("#add > div > div > div.modal-body").innerHTML = `<div class="container">
+				document.querySelector(".ng-binding").innerHTML = sl;
+			};
+			i.querySelector(".cart").onclick = function () {
+				let img = i.querySelector(".card-img-top").src;
+				let headingNote = i.querySelector(".heading-note").innerHTML;
+				let cardTitle = i.querySelector(".card-title").innerHTML;
+				let cardPrice = i.querySelector(".card-price").innerHTML;
+				document.querySelector("#buy-modal > div > div > div.modal-body").innerHTML = `<div class="container">
 				<div class="row">
 					<div class="col-6">
 						<img src="${img}" alt="" style="width:100%"/>
@@ -217,18 +200,338 @@ if (document.querySelectorAll(".creative .card")) {
 						<h3>${headingNote}</h3>
 						<h5>${cardTitle}</h5>
 						<p>${cardPrice}</p>
-						<button type="button" class="btn btn-danger" id="remove" 
-						data-bs-toggle="modal" data-bs-target="#buy-modal">remove</button>
-				<button id="buy" type="button" class="btn btn-primary">place Order</button>
+						<button type="button" class="btn btn-primary" id="add-card" 
+						data-bs-toggle="modal" data-bs-target="#add">Add To Card</button>
+				<button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#add">Buy</button>
 					</div>
 				</div>
 				
 			</div>
 				`;
+				document.querySelector(
+					"#buy-modal > div > div > div.modal-body > div > div > div:nth-child(2) > button.btn.btn-danger"
+				).onclick = function () {
+					document.querySelector("#add > div > div > div.modal-body").innerHTML = `
+					<div class="container">
+					<h3 class="text-center m-3">Shipping Details</h3>
+				<form action="#">
+					<div class="row">
+						<div class="col-3">
+							<label for="name">Name</label>
+							<br />
+							<label for="address">Address</label>
+							<br />
+							<label for="city">City/Suburb</label>
+							<br />
+							<label for="state">State</label>
+							<br />
+							<label for="code">Postal Code</label>
+							<br />
+							<label for="country">Country</label>
+						</div>
+						<div class="col-9">
+							<input type="text" name="name" />
+							<br />
+							<input type="text" name="address" />
+							<br />
+							<input type="text" name="city" />
+							<br />
+							<input type="text" name="state" />
+							<br />
+							<input type="text" name="code" />
+							<br />
+							<input type="text" name="country" />
+						</div>
+					</div>
+					<button type="button" class="btn btn-success m-5" data-bs-toggle="modal" data-bs-target="#buy-modal">Proceed to phayment</button>
+				</form>
+			</div>
+					`;
+					document.querySelector("#add > div > div > div.modal-body > div > form > button").onclick = buy();
+				};
+				document.querySelector("#add-card").onclick = function () {
+					document.querySelector("#add > div > div > div.modal-body").innerHTML = `<div class="container">
+					<div class="row">
+						<div class="col-6">
+							<img src="${img}" alt="" style="width:100%"/>
+						</div>
+						<div class="col-6">
+							<h3>${headingNote}</h3>
+							<h5>${cardTitle}</h5>
+							<p>${cardPrice}</p>
+							<button type="button" class="btn btn-danger" id="remove" 
+							data-bs-toggle="modal" data-bs-target="#buy-modal">remove</button>
+					<button id="buy" type="button" class="btn btn-primary"data-bs-toggle="modal" data-bs-target="#buy-modal">place Order</button>
+						</div>
+					</div>
+					
+				</div>
+					`;
+					document.querySelector("#buy").onclick = function () {
+						document.querySelector(
+							"#buy-modal > div > div > div.modal-body"
+						).innerHTML = `<div class="container">
+						<h3 class="text-center m-3">Shipping Details</h3>
+					<form action="#">
+						<div class="row">
+							<div class="col-3">
+								<label for="name">Name</label>
+								<br />
+								<label for="address">Address</label>
+								<br />
+								<label for="city">City/Suburb</label>
+								<br />
+								<label for="state">State</label>
+								<br />
+								<label for="code">Postal Code</label>
+								<br />
+								<label for="country">Country</label>
+							</div>
+							<div class="col-9">
+								<input type="text" name="name" />
+								<br />
+								<input type="text" name="address" />
+								<br />
+								<input type="text" name="city" />
+								<br />
+								<input type="text" name="state" />
+								<br />
+								<input type="text" name="code" />
+								<br />
+								<input type="text" name="country" />
+							</div>
+						</div>
+						<button type="button" class="btn btn-success m-5" data-bs-toggle="modal" data-bs-target="#add">Proceed to phayment</button>
+					</form>
+				</div>
+						`;
+						document.querySelector(
+							"#buy-modal > div > div > div.modal-body > div > form > button"
+						).onclick = function () {
+							document.querySelector(
+								"#add > div > div > div.modal-body"
+							).innerHTML = `<div class="container">
+							<form action="">
+								<div class="row">
+									<div class="col-3">
+										<label for="type"> Type </label>
+									</div>
+									<div class="col-9">
+										<select name="type" id="">
+											<option value="visa">Visa</option>
+											<option value="mastercard">mastercard</option>
+										</select>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-3">
+										<label for="number"> Number </label>
+									</div>
+									<div class="col-9"><input type="number" name="number" /></div>
+								</div>
+								<div class="row">
+									<div class="col-3">
+										<label for="date">Expriy Date</label>
+									</div>
+									<div class="col-9">
+										<input type="month" name="date" min="2022-03" value="2022-01" />
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-3">
+										<label for="name">Name on Card</label>
+									</div>
+									<div class="col-9">
+										<input type="text" name="name" />
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-3">
+										<input type="checkbox" name="paypal" />
+									</div>
+									<div class="col-9">
+										<label for="paypal">Paypal</label>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-3">
+										<input type="checkbox" name="cash" />
+									</div>
+									<div class="col-9">
+										<label for="cash">Cash on Delivery</label>
+									</div>
+								</div>
+							</form>
+							
+							<button type="button" class="btn btn-success m-3" data-bs-dismiss="modal">Confim Order</button>
+						</div>`;
+						};
+					};
+					document.querySelector("#remove").onclick = function () {
+						document.querySelector("#buy-modal > div > div > div.modal-body").innerHTML = `
+						<h3>No items show in the cart</h3>
+						<button type="button" class="btn btn-success" data-bs-dismiss="modal">continues to page</button>
+				`;
+					};
+				};
+				function buy() {
+					document.querySelector(
+						"#buy-modal > div > div > div.modal-body"
+					).innerHTML = `<div class="container">
+						<form action="">
+							<div class="row">
+								<div class="col-3">
+									<label for="type"> Type </label>
+								</div>
+								<div class="col-9">
+									<select name="type" id="">
+										<option value="visa">Visa</option>
+										<option value="mastercard">mastercard</option>
+									</select>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-3">
+									<label for="number"> Number </label>
+								</div>
+								<div class="col-9"><input type="number" name="number" /></div>
+							</div>
+							<div class="row">
+								<div class="col-3">
+									<label for="date">Expriy Date</label>
+								</div>
+								<div class="col-9">
+									<input type="month" name="date" min="2022-03" value="2022-01" />
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-3">
+									<label for="name">Name on Card</label>
+								</div>
+								<div class="col-9">
+									<input type="text" name="name" />
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-3">
+									<input type="checkbox" name="paypal" />
+								</div>
+								<div class="col-9">
+									<label for="paypal">Paypal</label>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-3">
+									<input type="checkbox" name="cash" />
+								</div>
+								<div class="col-9">
+									<label for="cash">Cash on Delivery</label>
+								</div>
+							</div>
+						</form>
+						
+						<button type="button" class="btn btn-success m-3" data-bs-dismiss="modal">Confim Order</button>
+					</div>`;
+				}
 			};
-			document.querySelector("#remove").onclick = function () {
-				document.querySelector("#buy-modal > div > div > div.modal-body").innerHTML = "hllo";
-			};
+		}
+	}
+}
+
+// ----------------------------------------------------------------
+if (document.querySelector("#download > h3")) {
+	document.querySelector("#download > h3").onclick = function () {
+		document.querySelector("#myModal > div > div > div.modal-body").innerHTML = `<div class="container">
+		<form action="" id="form-add">
+			<label for="name">Name</label>
+			<br />
+			<input type="text" name="name" />
+			<br />
+			<label for="img">Image</label>
+			<br />
+			<input type="text" name="img" />
+			<br />
+			<label for="price">Price</label>
+			<br />
+			<input type="text" name="price" />
+			<br />
+			<label for="category">Category</label>
+			<br />
+			<input type="text" name="category" />
+		</form>
+		<br />
+		<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Add</button>
+	</div>`;
+	};
+}
+if (document.querySelector("#bill-history > div > div > div > div > div")) {
+	let edit = document.querySelectorAll("#bill-history > div > div > div > div > div");
+	for (let i of edit) {
+		i.querySelector(".cart").onclick = function () {
+			let img = i.querySelector(".card-img-top").src;
+			let category = i.querySelector(".heading-note").innerHTML;
+			let cardTitle = i.querySelector(".card-title").innerHTML;
+			let cardPrice = i.querySelector(".card-price").innerHTML;
+			document.querySelector("#myModal > div > div > div.modal-body").innerHTML = `<div class="container">
+		<form action="" id="form-add">
+			<label for="name">Name</label>
+			<br />
+			<input type="text" name="name" value="${cardTitle}"/>
+			<br />
+			<label for="img">Image</label>
+			<br />
+			<input type="text" name="img"value="${img}"/>
+			<br />
+			<label for="price">Price</label>
+			<br />
+			<input type="text" name="price" value="${cardPrice}"/>
+			<br />
+			<label for="category">Category</label>
+			<br />
+			<input type="text" name="category" value="${category}"/>
+		</form>
+		<br />
+		<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Update</button>
+	</div>`;
 		};
 	}
 }
+if (document.querySelector("#edit-profile > div > div > div > div > div")) {
+	let edit = document.querySelectorAll("#edit-profile > div > div > div > div > div");
+	for (let i of edit) {
+		i.querySelector(".cart").onclick = function () {
+			let img = i.querySelector(".card-img-top").src;
+			let category = i.querySelector(".heading-note").innerHTML;
+			let cardTitle = i.querySelector(".card-title").innerHTML;
+			let cardPrice = i.querySelector(".card-price").innerHTML;
+			document.querySelector("#myModal > div > div > div.modal-body").innerHTML = `<div class="container">
+		<form action="" id="form-add">
+			<label for="name">Name</label>
+			<br />
+			<input type="text" name="name" value="${cardTitle}"/>
+			<br />
+			<label for="img">Image</label>
+			<br />
+			<input type="text" name="img"value="${img}"/>
+			<br />
+			<label for="price">Price</label>
+			<br />
+			<input type="text" name="price" value="${cardPrice}"/>
+			<br />
+			<label for="category">Category</label>
+			<br />
+			<input type="text" name="category" value="${category}"/>
+		</form>
+		<br />
+		<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Delete</button>
+	</div>`;
+		};
+	}
+}
+
+// angular
+
+var app = angular.module("myapp", []);
+app.controller("myctrl", function ($scope) {
+	$scope.a = "xinchao";
+});
